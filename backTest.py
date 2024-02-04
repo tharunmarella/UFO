ba03ca67eb7ae96b9dcde67f898c60e4efe8feb1
tharunmarella <tharunmarella@gmail.com>
2024-02-04 01:07:34 -0500

    Added Company Profile

--- backTest.py
+++ backTest.py
@@ -47,6 +47,21 @@ def get_prepared_price_data(chart_list):
     return price_doc
 
 
+def get_company_profile(company_symbol, apikey):
+    company_profile_url = 'https://financialmodelingprep.com/api/v3/profile/' + company_symbol + '?apikey=' + apikey
+    result = requests.get(company_profile_url).json()
+    return result
+
+
+def get_filtered_company_profile_doc(company_profile):
+    if company_profile:
+        company_profile = company_profile[0]
+        filtered_doc = {'cik': company_profile['cik'], 'country': company_profile['country'],
+                        'sector': company_profile['sector'], 'industry': company_profile['industry']}
+        return filtered_doc
+    return {}
+
+
 if __name__ == "__main__":
 
     resultList = []
@@ -61,10 +76,14 @@ if __name__ == "__main__":
             if marketcap_data and marketcap_data[0]['marketCap'] > billion:
                 company['date'] = datetime.strptime(company['date'], "%Y-%m-%d")
                 company_chart_list = get_historical_chart((company['date'] + timedelta(days=-7)).strftime("%Y-%m-%d"),
-                                                          (company['date']+ timedelta(days=-1)).strftime("%Y-%m-%d"),
+                                                          (company['date'] + timedelta(days=-1)).strftime("%Y-%m-%d"),
                                                           company['symbol'], API_KEY)
+
+                company_profile_doc = get_filtered_company_profile_doc(
+                    get_company_profile(company['symbol'], API_KEY))
                 price_gain_doc = get_prepared_price_data(company_chart_list)
-                resultList.append(company | {'marketCap': marketcap_data[0]['marketCap']} | price_gain_doc)
+                resultList.append(
+                    company | company_profile_doc | {'marketCap': marketcap_data[0]['marketCap']} | price_gain_doc)
                 print(resultList)
 
     earnings_collection.insert_many(resultList)
